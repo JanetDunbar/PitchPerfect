b763ab9dc4f6b0e4ebf684ab6ee46ba48751fe6c
jmd <jmd@amberlight.com>
2015-03-15 16:46:10 -0500

    Changed name of viewController to RecordSoundsViewController in 4 places.  Still works.

--- PitchPerfect/RecordSoundsViewController.swift
+++ PitchPerfect/RecordSoundsViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  RecordSoundsViewController.swift
 //  PitchPerfect
 //
 //  Created by Dr. Janet M. Dunbar on 3/12/15.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class RecordSoundsViewController: UIViewController {
 
     @IBOutlet weak var recordingInProgress: UILabel!
     @IBOutlet weak var stopButton: UIButton!
