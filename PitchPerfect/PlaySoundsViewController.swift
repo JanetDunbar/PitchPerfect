2296c1820eefc9d8896ac1eafa8849f12848e011
jmd <jmd@amberlight.com>
2015-03-15 19:55:52 -0500

    Now records your voice and plays it back at both speeds.

--- PitchPerfect/PlaySoundsViewController.swift
+++ PitchPerfect/PlaySoundsViewController.swift
@@ -13,19 +13,23 @@ import AVFoundation
 class PlaySoundsViewController: UIViewController {
     
     var audioPlayer:AVAudioPlayer!
+    var receivedAudio:RecordedAudio!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-        if var filePath = NSBundle.mainBundle().pathForResource("movie_quote", ofType: "mp3"){
-            //println("filePath is \(filePath)")
-            var filePathUrl = NSURL.fileURLWithPath(filePath)
-            //println("the url = \(url!)")
-            audioPlayer = AVAudioPlayer(contentsOfURL: filePathUrl, error: nil)
-            audioPlayer.enableRate = true
-        } else{
-            println("The file path is empty.")
-        }
+//        if var filePath = NSBundle.mainBundle().pathForResource("movie_quote", ofType: "mp3"){
+//            var filePathUrl = NSURL.fileURLWithPath(filePath)
+//            //println("the url = \(url!)")
+//            
+//        
+//        } else{
+//            println("The file path is empty.")
+//        }
+        
+        audioPlayer = AVAudioPlayer(contentsOfURL: receivedAudio.filePathUrl, error: nil)
+        audioPlayer.enableRate = true
     }
 
     override func didReceiveMemoryWarning() {
